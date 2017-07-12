f0d91752c9cf5964832491b7ee74fb85017283df
Max Ogden <max@maxogden.com>
2017-07-11 18:02:33 -0700

    parse responses

--- index.js
+++ index.js
@@ -1,100 +1,110 @@
-module.exports = function (handle) {
-  // OpFlag masks
-  var MSG_FLAG_AUTH = 0x80 // don't use cache, use only primaries
-  var MSG_FLAG_CERT = 0x40 // asks server to sign responses
-  var MSG_FLAG_ENCR = 0x20 // asks server to encrypt responses
-  var MSG_FLAG_RECU = 0x10 // server should try and resolve handle if not found
-  var MSG_FLAG_CACR = 0x08 // responses should be signed by cache
-  var MSG_FLAG_CONT = 0x04 // there are more parts to this message
-  var MSG_FLAG_KPAL = 0x02 // keep the socket open for more requests
-  var MSG_FLAG_PUBL = 0x01 // resolution requests should only return public vals
-  var MSG_FLAG_RRDG = 0x80 // responses should include a digest of the request
-  var MSG_FLAG_AUTH_INDEX = 0
-  var MSG_FLAG_CERT_INDEX = 0
-  var MSG_FLAG_ENCR_INDEX = 0
-  var MSG_FLAG_RECU_INDEX = 0
-  var MSG_FLAG_CACR_INDEX = 0
-  var MSG_FLAG_CONT_INDEX = 0
-  var MSG_FLAG_KPAL_INDEX = 0
-  var MSG_FLAG_PUBL_INDEX = 0
-  var MSG_FLAG_RRDG_INDEX = 1
+// thanks to https://github.com/cul/cul-handles for inspiration
 
-  // MessageFlag masks
-  var ENV_FLAG_COMPRESSED = 0x80
-  var ENV_FLAG_ENCRYPTED = 0x40
-  var ENV_FLAG_TRUNCATED = 0x20
+var net = require('net')
+var debug = require('debug')('handle-protocol')
+var concat = require('concat-stream')
 
-  // OpCode values
-  var OC_RESERVED = 0
-  var OC_RESOLUTION = 1
-  var OC_GET_SITEINFO = 2
-  var OC_CREATE_HANDLE = 100
-  var OC_DELETE_HANDLE = 101
-  var OC_ADD_VALUE = 102
-  var OC_REMOVE_VALUE = 103
-  var OC_MODIFY_VALUE = 104
-  var OC_LIST_HANDLE = 105
-  var OC_LIST_NA = 106
-  var OC_CHALLENGE_RESPONSE = 200
-  var OC_VERIFY_RESPONSE = 201
-  var OC_SESSION_SETUP = 400
-  var OC_SESSION_TERMINATE = 401
-  var OC_SESSION_EXCHANGEKEY = 402
+// OpFlag masks
+var MSG_FLAG_AUTH = 0x80 // don't use cache, use only primaries
+var MSG_FLAG_CERT = 0x40 // asks server to sign responses
+var MSG_FLAG_ENCR = 0x20 // asks server to encrypt responses
+var MSG_FLAG_RECU = 0x10 // server should try and resolve handle if not found
+var MSG_FLAG_CACR = 0x08 // responses should be signed by cache
+var MSG_FLAG_CONT = 0x04 // there are more parts to this message
+var MSG_FLAG_KPAL = 0x02 // keep the socket open for more requests
+var MSG_FLAG_PUBL = 0x01 // resolution requests should only return public vals
+var MSG_FLAG_RRDG = 0x80 // responses should include a digest of the request
+var MSG_FLAG_AUTH_INDEX = 0
+var MSG_FLAG_CERT_INDEX = 0
+var MSG_FLAG_ENCR_INDEX = 0
+var MSG_FLAG_RECU_INDEX = 0
+var MSG_FLAG_CACR_INDEX = 0
+var MSG_FLAG_CONT_INDEX = 0
+var MSG_FLAG_KPAL_INDEX = 0
+var MSG_FLAG_PUBL_INDEX = 0
+var MSG_FLAG_RRDG_INDEX = 1
 
-  // ResponseCode values
-  var RC_RESERVED = 0
-  var RC_SUCCESS = 1
-  var RC_ERROR = 2
-  var RC_SERVER_BUSY = 3
-  var RC_PROTOCOL_ERROR = 4
-  var RC_OPERATION_DENIED = 5
-  var RC_RECUR_LIMIT_EXCEEDED = 6
-  var RC_HANDLE_NOT_FOUND = 100
-  var RC_HANDLE_ALREADY_EXIST = 101
-  var RC_INVALID_HANDLE = 102
-  var RC_VALUE_NOT_FOUND = 200
-  var RC_VALUE_ALREADY_EXIST = 201
-  var RC_VALUE_INVALID = 202
-  var RC_EXPIRED_SITE_INFO = 300
-  var RC_SERVER_NOT_RESP = 301
-  var RC_SERVICE_REFERRAL = 302
-  var RC_NA_DELEGATE = 303
-  var RC_NOT_AUTHORIZED = 400
-  var RC_ACCESS_DENIED = 401
-  var RC_AUTHEN_NEEDED = 402
-  var RC_AUTHEN_FAILED = 403
-  var RC_INVALID_CREDENTIAL = 404
-  var RC_AUTHEN_TIMEOUT = 405
-  var RC_UNABLE_TO_AUTHEN = 406
-  var RC_SESSION_TIMEOUT = 500
-  var RC_SESSION_FAILED = 501
-  var RC_NO_SESSION_KEY = 502
-  var RC_SESSION_NO_SUPPORT = 503
-  var RC_SESSION_KEY_INVALID = 504
-  var RC_TRYING = 900
-  var RC_FORWARDED = 901
-  var RC_QUEUED = 902
+// MessageFlag masks
+var ENV_FLAG_COMPRESSED = 0x80
+var ENV_FLAG_ENCRYPTED = 0x40
+var ENV_FLAG_TRUNCATED = 0x20
 
-  // handle value admin permissions
-  var PERM_ADD_HANDLE = 0x0001
-  var PERM_DELETE_HANDLE = 0x0002
-  var PERM_ADD_NA = 0x0004
-  var PERM_DELETE_NA = 0x0008
-  var PERM_MODIFY_VALUE = 0x0010
-  var PERM_REMOVE_VALUE = 0x0020
-  var PERM_ADD_VALUE = 0x0040
-  var PERM_MODIFY_ADMIN = 0x0080
-  var PERM_REMOVE_ADMIN = 0x0100
-  var PERM_ADD_ADMIN = 0x0200
-  var PERM_READ_VALUE = 0x0400
-  var PERM_LIST_HDLS = 0x0800
-  var PERM_ALL = 0x0fff
+// OpCode values
+var OC_RESERVED = 0
+var OC_RESOLUTION = 1
+var OC_GET_SITEINFO = 2
+var OC_CREATE_HANDLE = 100
+var OC_DELETE_HANDLE = 101
+var OC_ADD_VALUE = 102
+var OC_REMOVE_VALUE = 103
+var OC_MODIFY_VALUE = 104
+var OC_LIST_HANDLE = 105
+var OC_LIST_NA = 106
+var OC_CHALLENGE_RESPONSE = 200
+var OC_VERIFY_RESPONSE = 201
+var OC_SESSION_SETUP = 400
+var OC_SESSION_TERMINATE = 401
+var OC_SESSION_EXCHANGEKEY = 402
 
-  // standard handle indices
-  var INDEX_ADMIN_HANDLE = 100 // index for create/delete/super admin
-  var INDEX_MAINTAINER_HANDLE = 101 // index for modify/update admin
-  var INDEX_AUTH = 300 // index of HS_SECKEY value
+// ResponseCode values
+var RC_RESERVED = 0
+var RC_SUCCESS = 1
+var RC_ERROR = 2
+var RC_SERVER_BUSY = 3
+var RC_PROTOCOL_ERROR = 4
+var RC_OPERATION_DENIED = 5
+var RC_RECUR_LIMIT_EXCEEDED = 6
+var RC_HANDLE_NOT_FOUND = 100
+var RC_HANDLE_ALREADY_EXIST = 101
+var RC_INVALID_HANDLE = 102
+var RC_VALUE_NOT_FOUND = 200
+var RC_VALUE_ALREADY_EXIST = 201
+var RC_VALUE_INVALID = 202
+var RC_EXPIRED_SITE_INFO = 300
+var RC_SERVER_NOT_RESP = 301
+var RC_SERVICE_REFERRAL = 302
+var RC_NA_DELEGATE = 303
+var RC_NOT_AUTHORIZED = 400
+var RC_ACCESS_DENIED = 401
+var RC_AUTHEN_NEEDED = 402
+var RC_AUTHEN_FAILED = 403
+var RC_INVALID_CREDENTIAL = 404
+var RC_AUTHEN_TIMEOUT = 405
+var RC_UNABLE_TO_AUTHEN = 406
+var RC_SESSION_TIMEOUT = 500
+var RC_SESSION_FAILED = 501
+var RC_NO_SESSION_KEY = 502
+var RC_SESSION_NO_SUPPORT = 503
+var RC_SESSION_KEY_INVALID = 504
+var RC_TRYING = 900
+var RC_FORWARDED = 901
+var RC_QUEUED = 902
 
+// handle value admin permissions
+var PERM_ADD_HANDLE = 0x0001
+var PERM_DELETE_HANDLE = 0x0002
+var PERM_ADD_NA = 0x0004
+var PERM_DELETE_NA = 0x0008
+var PERM_MODIFY_VALUE = 0x0010
+var PERM_REMOVE_VALUE = 0x0020
+var PERM_ADD_VALUE = 0x0040
+var PERM_MODIFY_ADMIN = 0x0080
+var PERM_REMOVE_ADMIN = 0x0100
+var PERM_ADD_ADMIN = 0x0200
+var PERM_READ_VALUE = 0x0400
+var PERM_LIST_HDLS = 0x0800
+var PERM_ALL = 0x0fff
+
+// standard handle indices
+var INDEX_ADMIN_HANDLE = 100 // index for create/delete/super admin
+var INDEX_MAINTAINER_HANDLE = 101 // index for modify/update admin
+var INDEX_AUTH = 300 // index of HS_SECKEY value
+
+module.exports = function (handle, options, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
   var majorVersion = 2
   var minorVersion = 1
   var digestAlg = 2 // SHA1 = 2, MD5 = 1
@@ -156,15 +166,218 @@ module.exports = function (handle) {
   envelope = envelope.concat(asBytes(requestId))
   envelope = envelope.concat(sequenceNumber)
   envelope = envelope.concat(asBytes(body.length + 24 + credential.length))
-  
+
   var packet = [].concat(envelope)
   packet = packet.concat(header)
   packet = packet.concat(body)
   packet = packet.concat(credential)
-  
-  console.log('env', envelope.length, 'header', header.length, 'body', body.length, 'creds', credential.length, 'packet', packet.length)
-  
-  return new Buffer(packet)
+
+  debug('env', envelope.length, 'header', header.length, 'body', body.length, 'creds', credential.length, 'packet', packet.length)
+
+  packet = new Buffer(packet)
+
+  var GHRs = [ // from root_info.c
+    '132.151.20.9', // Root Mirror #3 at CNRI
+    '38.100.138.131', // root primary
+    '63.123.152.246', // crossref global mirror
+    '132.151.1.179' // east coast root mirror
+  ]
+
+  var HOST = options.host || GHRs[2]
+  var PORT = options.port || 2641
+
+  var socket = net.connect(PORT, HOST)
+  socket.on('connect', function () {
+    debug('tcp socket connected', HOST, PORT)
+    socket.write(packet)
+    socket.end()
+    socket.pipe(concat(function (data) {
+      var envelope = parseEnvelope(data.slice(0, 20))
+      var header = parseHeader(data.slice(20, 44))
+      var body = parseBody(header, data.slice(44, 44 + header.bodyLength))
+      // TODO var credential = parseCredential()
+      var values = parseValues(body.body)
+      var resp = {
+        header: header,
+        values: values.values,
+        handle: values.handle
+      }
+      cb(null, resp)
+    }))
+  })
+
+  socket.on('error', function (err) {
+    cb(err)
+  })
+
+  function parseEnvelope (buf) {
+    var envelope = {}
+    envelope.majorVersion = buf.slice(0, 1).readUInt8()
+    envelope.minorVersion = buf.slice(1, 2).readUInt8()
+    envelope.mflag = buf.slice(2, 4)
+    var mflag = envelope.mflag.readUInt8()
+    if ((mflag & ENV_FLAG_COMPRESSED) === ENV_FLAG_COMPRESSED) envelope.compressed = true
+    if ((mflag & ENV_FLAG_ENCRYPTED) === ENV_FLAG_ENCRYPTED) envelope.encrypted = true
+    if ((mflag & ENV_FLAG_TRUNCATED) === ENV_FLAG_TRUNCATED) envelope.truncated = true
+    envelope.sessionId = buf.slice(4, 8).readUInt32BE()
+    envelope.requestId = buf.slice(8, 12).readUInt32BE()
+    envelope.sequenceNumber = buf.slice(12, 16).readUInt32BE()
+    envelope.messageLength = buf.slice(16, 20).readUInt32BE()
+    return envelope
+  }
+
+  function parseHeader (buf) {
+    var header = {}
+    header.opCode = buf.slice(0, 4).readUInt32BE()
+    header.responseCode = buf.slice(4, 8).readUInt32BE()
+    header.opFlag = buf.slice(8, 12).readUInt32BE()
+    header.siteInfoSerial = buf.slice(12, 14).readUInt16BE()
+    header.recursionCount = buf.slice(14, 15).readUInt8()
+    header.expirationTime = buf.slice(16, 20).readUInt32BE()
+    header.bodyLength = buf.slice(20, 24).readUInt32BE()
+    return header
+  }
+
+  function parseBody (header, buf) {
+    var body = {}
+    var offset = 0
+    if (header.opFlag[MSG_FLAG_RRDG_INDEX] & MSG_FLAG_RRDG > 0) {
+      var alg = buf.slice(0, 1).readUInt8()
+      body.digestAlgorithm = alg
+      var digestLen = 0
+      if (alg === 1) digestLen = 16
+      if (alg === 2) digestLen = 20
+      if (digestLen) {
+        offset = 1 + digestLen
+        body.digest = buf.slice(1, offset)
+      }
+    }
+    body.body = buf.slice(offset)
+    return body
+  }
+
+  function parseValues (buf) {
+    var values = {values: []}
+    var handleLen = buf.slice(0, 4).readUInt32BE()
+    var offset = 4 + handleLen
+    var handle = buf.slice(4, offset).toString()
+    debug('parseValues handleLen, handle', handleLen, handle)
+    var numVals = buf.slice(offset, offset + 4)
+    offset = offset + 4
+    values.handle = handle
+    if (numVals.length !== 4) return
+    numVals = numVals.readUInt32BE()
+    debug('parseValues numVals', numVals)
+    for (var i = 0; i < numVals; i++) {
+      var vals = buf.slice(offset)
+      var valueLength = calculateValueLength(vals)
+      debug('valueLength', valueLength)
+      var value = vals.slice(0, valueLength)
+      var parsed = parseValue(value)
+      values.values.push(parsed)
+      offset = offset + valueLength
+    }
+    return values
+  }
+
+  function parseValue (buf) {
+    var val = {refs: []}
+    val.index = buf.slice(0, 4).readUInt32BE()
+    val.timestamp = buf.slice(4, 8).readUInt32BE()
+    val.ttlType = buf.slice(8, 9).readUInt8()
+    val.ttl = buf.slice(9, 13).readUInt32BE()
+    val.perm = buf.slice(13, 14).readUInt8()
+    var typeLength = buf.slice(14, 18).readUInt32BE()
+    val.type = buf.slice(18, 18 + typeLength).toString()
+    var offset = 18 + typeLength
+    var dataLength = buf.slice(offset, offset + 4).readUInt32BE()
+    offset = offset + 4
+    val.data = buf.slice(offset, offset + dataLength)
+    if (val.type === 'HS_SITE') {
+      val.data = parseHSSite(val.data)
+    }
+    if (val.type === 'URL') {
+      val.data = val.data.toString()
+    }
+    offset = offset + dataLength
+    var refsLength = buf.slice(offset, offset + 4).readUInt32BE()
+    debug('refsLength', refsLength)
+    offset = offset + 4
+    for (var i = 0; i < refsLength; i++) {
+      val.refs.push(buf.slice(offset, offset + 4).readUInt32BE())
+      offset = offset + 4
+    }
+    return val
+  }
+
+  function parseHSSite (buf) {
+    var site = {attrs: {}, serverRecords: []}
+    site.version = buf.slice(0, 2)
+    site.protocolVersion = buf.slice(2, 4)
+    site.serialNumber = buf.slice(4, 6)
+    site.primaryMask = buf.slice(6, 7)
+    site.hashOption = buf.slice(7, 8)
+    var hashFilterLength = buf.slice(8, 12).readUInt32BE()
+    debug('hashFilterLength', hashFilterLength)
+    var offset = 12 + hashFilterLength
+    var numAttrs = buf.slice(offset, offset + 4).readUInt32BE()
+    offset = offset + 4
+    debug('numAttrs', numAttrs)
+    for (var i = 0; i < numAttrs; i++) {
+      var keyLen = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4
+      var key = buf.slice(offset, offset + keyLen).toString()
+      offset = offset + keyLen
+      var valueLen = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4
+      var value = buf.slice(offset, offset + valueLen).toString()
+      offset = offset + valueLen
+      site.attrs[key] = value
+    }
+    var serverCount = buf.slice(offset, offset + 4).readUInt32BE()
+    offset = offset + 4
+    debug('serverCount', serverCount)
+    for (var i = 0; i < serverCount; i++) {
+      var record = {serviceInterfaces: []}
+      record.id = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4
+      record.address = buf.slice(offset, offset + 16)
+      // HACK disregards ipv6
+      record.address = record.address.slice(12, 16).join('.')
+      offset = offset + 16
+      var pubkeyLength = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4
+      record.publicKey = buf.slice(offset, offset + pubkeyLength)
+      offset = offset + pubkeyLength
+      var interfaceCount = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4
+      for (var j = 0; j < interfaceCount; j++) {
+        var iface = {}
+        iface.serviceType = buf.slice(offset, offset + 1)
+        iface.transmissionProtocol = buf.slice(offset + 1, offset + 2)
+        iface.portNumber = buf.slice(offset + 2, offset + 6).readUInt32BE()
+        record.serviceInterfaces.push(iface)
+        offset = offset + 6
+      }
+      site.serverRecords.push(record)
+    }
+    return site
+  }
+
+  function calculateValueLength (buf) {
+    var offset = 14 // index - 4 bytes; timestamp - 4 bytes; ttlType - 1 byte; ttl - 4 bytes; permissions - 1 byte
+    var fieldLength = buf.slice(offset, offset + 4).readUInt32BE() // type field
+    offset = offset + 4 + fieldLength
+    var fieldLength = buf.slice(offset, offset + 4).readUInt32BE() // data field
+    offset = offset + 4 + fieldLength
+    var fieldLength = buf.slice(offset, offset + 4).readUInt32BE() // references (number of)
+    offset = offset + 4 + fieldLength
+    for (var i = 0; i < fieldLength; i++) {
+      var refLength = buf.slice(offset, offset + 4).readUInt32BE()
+      offset = offset + 4 + refLength + 4
+    }
+    return offset
+  }
 
   function toProtocolString (buf) {
     if (!Buffer.isBuffer(buf)) buf = new Buffer(buf)
@@ -176,7 +389,7 @@ module.exports = function (handle) {
 
   function asBytes (val) {
     var i = new Buffer(4)
-    console.log('write', val)
+    debug('asBytes', val)
     i.writeUInt32BE(val, 0)
     var bytes = JSON.parse('[' + i.join(',') + ']')
     return bytes
--- test.js
+++ test.js
@@ -1,26 +1,23 @@
 var net = require('net')
 var Handle = require('./')
-var hnd = "10.1016/j.toxicon.2016.11.113"
-var prefix = "10.1016"
-var GHRname = "0.NA/10.1016"
-var req = Handle(GHRname)
 
-var GHR = [ // from root_info.c
-  '132.151.20.9', // Root Mirror #3 at CNRI
-  '38.100.138.131', // root primary
-  '63.123.152.246', // crossref global mirror
-  '132.151.1.179' // east coast root mirror
-]
+var handle = "10.1016/j.toxicon.2016.11.113"
+var xref = "10.SERV/CROSSREF"
+var ONA = "0.NA/" + handle.split('/')[0]
+var doiOak = '38.100.138.133'
+var hybrid = '0.NA/10.SERV'
 
-var socket = net.connect(2641, GHR[2])
-socket.on('connect', function () {
-  console.log('connected')
-  socket.write(req)
-  socket.end()
-  socket.on('data', function (d) {
-    console.log('resp', d.toString())
-  })
-})
-socket.on('error', function (err) {
-  throw err
+// Handle(hybrid, function (err, data) {
+//   data.values.values.forEach(function (val) {
+//     console.log(val)
+//     if (val.data) console.log(JSON.stringify(val.data, null, '  '))
+//   })
+// })
+
+// Handle(xref, {host: '38.100.138.133'}, function (err, data) {
+//   console.log(JSON.stringify(data.values.values, null, '  '))
+// })
+
+Handle(handle, {host: '208.254.38.90'}, function (err, data) {
+  console.log(JSON.stringify(data, null, '  '))
 })
